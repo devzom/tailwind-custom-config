519beb5f3bbfc4923202f0c1ea2c1287bad55daf
Jakub Zomerfeld <jakub.zomerfeld@gmail.com>
2022-07-16 18:30:04 +0200

    feat: specify primary & secondary colors, increase pkg ver

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 module.exports = {
     theme: {
         colors: {
-            blue: {
-                light: '#85d7ff',
-                DEFAULT: '#1fb6ff',
-                dark: '#009eeb',
+            primary: {
+                50: '#f8fafc',
+                100: '#f1f5f9',
+                200: '#e2e8f0',
+                300: '#cbd5e1',
+                400: '#94a3b8',
+                500: '#64748b',
+                600: '#475569',
+                700: '#334155',
+                800: '#1e293b',
+                900: '#0f172a',
             },
-            pink: {
-                light: '#ff7ce5',
-                DEFAULT: '#ff49db',
-                dark: '#ff16d1',
+            secondary: {
+                50: '#ecfdf5',
+                100: '#d1fae5',
+                200: '#a7f3d0',
+                300: '#6ee7b7',
+                400: '#34d399',
+                500: '#10b981',
+                600: '#059669',
+                700: '#047857',
+                800: '#065f46',
+                900: '#064e3b',
             },
-            gray: {
-                darkest: '#1f2d3d',
-                dark: '#3c4858',
-                DEFAULT: '#c0ccda',
-                light: '#e0e6ed',
-                lightest: '#f9fafc',
-            }
         },
         fontFamily: {
             sans: ['Graphik', 'sans-serif'],
